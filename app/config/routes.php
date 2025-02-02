444aa8467fd88cdf93e2194bda198a8d3d726a11
Fanekena-dev <fanekenaraharijaona@gmail.com>
2025-02-02 08:04:28 +0100

    added landing page route configuration

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,9 +2,12 @@
 
 use flight\Engine;
 use flight\net\Router;
-//use Flight;
+
+use app\controllers\LandingController;
 
 /** 
  * @var Router $router 
  * @var Engine $app
  */
+
+$router->get('/', [new LandingController(), 'landingPage']);
